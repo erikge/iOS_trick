8a039dbf03be59f0e534be37c8e1fe47c62dc572
eric <erikge@qq.com>
2013-12-05 16:34:33 +0800

    采用 kstenerud 的模板创建通用framework

---
+++ staticlib_workspace/lib-outof-workspace/finalUniByTemplate/finalUniByTemplate.h
@@ -0,0 +1,13 @@
+//
+//  finalUniByTemplate.h
+//  finalUniByTemplate
+//
+//  Created by VCTL on 13-12-5.
+//  Copyright (c) 2013年 erikge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface finalUniByTemplate : NSObject
+
+@end
---
+++ staticlib_workspace/lib-outof-workspace/finalUniByTemplate/finalUniByTemplate.m
@@ -0,0 +1,13 @@
+//
+//  finalUniByTemplate.m
+//  finalUniByTemplate
+//
+//  Created by VCTL on 13-12-5.
+//  Copyright (c) 2013年 erikge. All rights reserved.
+//
+
+#import "finalUniByTemplate.h"
+
+@implementation finalUniByTemplate
+
+@end
